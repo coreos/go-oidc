3018b7d0b02c79bd970a0d9598468c1b71fdb2b7
Eric Chiang <eric.chiang.m@gmail.com>
2016-03-03 12:09:28 -0800

    *: add simplified client that uses existing oauth2 and jose packages

---
+++ client.go
@@ -0,0 +1,182 @@
+package oidc
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/square/go-jose"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+)
+
+// ScopeOpenID is the mandatory scope for all OpenID Connect OAuth2 requests.
+const ScopeOpenID = "openid"
+
+// TokenVerifier uses public keys to verify a JWT.
+type TokenVerifier interface {
+	// Verify verifies at least one of the signatures of the JWS and returns the
+	// payload associated with JSON Web Token.
+	Verify(jws *jose.JsonWebSignature) (payload []byte, err error)
+}
+
+type IDToken struct {
+	Issuer   string   `json:"iss"`
+	Subject  string   `json:"sub"`
+	Audience string   `json:"aud"`
+	Exp      int64    `json:"exp"`
+	Iat      int64    `json:"iat"`
+	AuthTime int64    `json:"auth_time,omitempty"`
+	Nonce    string   `json:"string,omitempty"`
+	ACR      string   `json:"acr,omitempty"`
+	AMR      []string `json:"amr,omitempty"`
+	AZP      string   `json:"azp,omitempty"`
+}
+
+// ParseIDToken extracts the OpenID Connect ID Token from an OAuth2 token.
+// Is uses the provided TokenVerifier to verify that the token has been signed by the
+// appropriate source.
+func ParseIDToken(v TokenVerifier, t *oauth2.Token) (*IDToken, error) {
+	val := t.Extra("id_token")
+	if val == nil {
+		return nil, errors.New("oidc: no id_token field in token")
+	}
+	s, ok := val.(string)
+	if !ok {
+		return nil, errors.New("oidc: id_token field not a string")
+	}
+	jws, err := jose.ParseSigned(s)
+	if err != nil {
+		return nil, fmt.Errorf("oidc: failed to parse id_token: %v", err)
+	}
+	payload, err := v.Verify(jws)
+	if err != nil {
+		return nil, err
+	}
+	var tok IDToken
+	if err := json.Unmarshal(payload, &tok); err != nil {
+		return nil, fmt.Errorf("oidc: failed to unmarshal token payload: %v", err)
+	}
+	return &tok, nil
+}
+
+type Provider struct {
+	Issuer      string   `json:"issuer"`
+	AuthURL     string   `json:"authorization_endpoint"`
+	TokenURL    string   `json:"token_endpoint"`
+	JWKSURL     string   `json:"jwks_uri"`
+	UserInfoURL string   `json:"userinfo_endpoint"`
+	Scopes      []string `json:"scopes_supported"`
+}
+
+// NewProvider uses the OpenID Connect disovery mechanism to construct a Provider.
+//
+// issuer should be a URL with only a scheme, host, and optional port.
+func NewProvider(ctx context.Context, issuer string) (*Provider, error) {
+	u, err := url.Parse(issuer)
+	if err != nil {
+		return nil, fmt.Errorf("oidc: failed to parse issuer as URL: %v", err)
+	}
+	if u.Path != "" {
+		return nil, fmt.Errorf("oidc: issuer is expected to be scheme and host without a path")
+	}
+	u.Path = "/.well-known/openid-configuration"
+
+	cli := contextClient(ctx)
+	resp, err := cli.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var p Provider
+	if err := json.NewDecoder(resp.Body).Decode(&p); err != nil {
+		return nil, fmt.Errorf("oidc: failed to decode provider at well known config: %v", err)
+	}
+	return &p, nil
+}
+
+// Endpoint returns the OAuth2 auth and token endpoints for the given provider.
+func (p *Provider) Endpoint() oauth2.Endpoint {
+	return oauth2.Endpoint{AuthURL: p.AuthURL, TokenURL: p.TokenURL}
+}
+
+// UserInfo represents the OpenID Connect userinfo claims.
+type UserInfo struct {
+	Subject       string
+	Profile       string
+	Email         string
+	EmailVerified bool
+
+	// Optionally contains extra claims.
+	rawClaims map[string]interface{}
+}
+
+// Extra returns additional claims returned by the server.
+func (u *UserInfo) Extra(key string) interface{} {
+	if u.rawClaims != nil {
+		return u.rawClaims[key]
+	}
+	return nil
+}
+
+// UserInfo uses the token source to query the provider's userinfo endpoint.
+func (p *Provider) UserInfo(ctx context.Context, tokenSource oauth2.TokenSource) (*UserInfo, error) {
+	if p.UserInfoURL == "" {
+		return nil, errors.New("oidc: provider doid not provide a userinfo endpoint")
+	}
+	cli := oauth2.NewClient(ctx, tokenSource)
+	resp, err := cli.Get(p.UserInfoURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s: %s", resp.Status, body)
+	}
+
+	var userinfo struct {
+		Subject       string `json:"sub"`
+		Profile       string `json:"profile"`
+		Email         string `json:"email"`
+		EmailVerified bool   `json:"email_verified"`
+	}
+	var raw map[string]interface{}
+
+	if err := json.Unmarshal(body, &userinfo); err != nil {
+		return nil, fmt.Errorf("oidc: failed to decode userinfo: %v", err)
+	}
+	if err := json.Unmarshal(body, &raw); err != nil {
+		return nil, fmt.Errorf("oidc: failed to decode userinfo: %v", err)
+	}
+	return &UserInfo{
+		Subject:       userinfo.Subject,
+		Profile:       userinfo.Profile,
+		Email:         userinfo.Email,
+		EmailVerified: userinfo.EmailVerified,
+		rawClaims:     raw,
+	}, nil
+}
+
+// Verifier returns a TokenVerifier that uses the provider's key set to verify JWTs.
+//
+// The verifier queries the provider to update keys when a signature cannot be verified by the
+// set of keys cached from the previous request.
+func (p *Provider) Verifier(ctx context.Context) TokenVerifier {
+	return newRemoteKeySet(ctx, p.JWKSURL)
+}
+
+func contextClient(ctx context.Context) *http.Client {
+	if ctx != nil {
+		if hc, ok := ctx.Value(oauth2.HTTPClient).(*http.Client); ok {
+			return hc
+		}
+	}
+	return http.DefaultClient
+}
---
+++ remote_jwks.go
@@ -0,0 +1,150 @@
+package oidc
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/square/go-jose"
+	"golang.org/x/net/context"
+)
+
+type keyRequest struct {
+	jws    *jose.JsonWebSignature
+	respCh chan keyResponse
+}
+
+type keyResponse struct {
+	key interface{}
+	err error
+}
+
+type remoteKeySet struct {
+	ctx     context.Context
+	hc      *http.Client
+	jwksURL string
+
+	requestCh chan keyRequest
+}
+
+func newRemoteKeySet(ctx context.Context, jwksURL string) *remoteKeySet {
+	r := &remoteKeySet{
+		ctx:       ctx,
+		hc:        contextClient(ctx),
+		jwksURL:   jwksURL,
+		requestCh: make(chan keyRequest),
+	}
+	go r.start() // begin the main loop in a goroutine.
+	return r
+}
+
+func (r *remoteKeySet) Verify(jws *jose.JsonWebSignature) (payload []byte, err error) {
+	// Construct a key requests.
+	keyReq := keyRequest{jws, make(chan keyResponse, 1)}
+
+	select {
+	case r.requestCh <- keyReq: // Send the request on the requests channel.
+	case <-r.ctx.Done():
+		return nil, r.ctx.Err()
+	}
+
+	select {
+	case resp := <-keyReq.respCh: // Wait for a response on the response channel.
+		if resp.err != nil {
+			return nil, resp.err
+		}
+		return jws.Verify(resp.key)
+	case <-r.ctx.Done():
+		return nil, r.ctx.Err()
+	}
+}
+
+// updateKeys queries the provider for the current set of keys.
+func (r *remoteKeySet) updateKeys() (jose.JsonWebKeySet, error) {
+	resp, err := r.hc.Get(r.jwksURL)
+	if err != nil {
+		return jose.JsonWebKeySet{}, err
+	}
+	defer resp.Body.Close()
+
+	var set jose.JsonWebKeySet
+	if err := json.NewDecoder(resp.Body).Decode(&set); err != nil {
+		return jose.JsonWebKeySet{}, fmt.Errorf("failed to decode key set: %v", err)
+	}
+	return set, nil
+}
+
+func keyForSet(keys map[string]interface{}, jws *jose.JsonWebSignature) (interface{}, bool) {
+	for _, sig := range jws.Signatures {
+		if key, ok := keys[sig.Header.KeyID]; ok {
+			return key, true
+		}
+	}
+	return nil, false
+}
+
+type keySetResponse struct {
+	keyset jose.JsonWebKeySet
+	err    error
+}
+
+// start beings the main loop.
+func (r *remoteKeySet) start() {
+	remoteResponseCh := make(chan keySetResponse, 1) // Channel to wait for a remote response on.
+	var waitingReqs []keyRequest                     // Request that are waiting for a remote response.
+	keys := make(map[string]interface{})             // Keys currently cached by the key set.
+
+	for {
+		select {
+		case req := <-r.requestCh: // Got a request for a key from a goroutine.
+			if key, ok := keyForSet(keys, req.jws); ok {
+				// A key is cached that can verify the JWS.
+				req.respCh <- keyResponse{key, nil}
+				break
+			}
+
+			if len(waitingReqs) == 0 {
+				// No current inflight request, make a request.
+				go func() {
+					set, err := r.updateKeys()
+					remoteResponseCh <- keySetResponse{set, err}
+				}()
+			}
+			// Add request to the wait queue.
+			waitingReqs = append(waitingReqs, req)
+
+		case resp := <-remoteResponseCh:
+			// A request to the provider came back with new keys.
+			// Respond to all queued requests.
+
+			if resp.err != nil {
+				for _, req := range waitingReqs {
+					req.respCh <- keyResponse{nil, resp.err}
+				}
+			} else {
+				// reset the keys
+				keys = make(map[string]interface{})
+				for _, key := range resp.keyset.Keys {
+					keys[key.KeyID] = key.Key
+				}
+
+				// for each waiting request, respond.
+				for _, req := range waitingReqs {
+					var keyResp keyResponse
+					if key, ok := keyForSet(keys, req.jws); ok {
+						keyResp.key = key
+					} else {
+						keyResp.err = errors.New("key not found")
+					}
+					req.respCh <- keyResp
+				}
+			}
+
+			waitingReqs = nil // Clear wait queue.
+
+		case <-r.ctx.Done():
+			return
+		}
+	}
+}
