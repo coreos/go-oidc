810f6f074a76a094290accea67eba21334c8233b
Joshua Carp <jm.carp@gmail.com>
2017-10-06 17:15:44 -0400

    Skip issuser verification.
    [Resolves #159]

--- oidc.go
+++ oidc.go
@@ -111,9 +111,6 @@ func NewProvider(ctx context.Context, issuer string) (*Provider, error) {
 		return nil, fmt.Errorf("oidc: failed to decode provider discovery object: %v", err)
 	}
 
-	if p.Issuer != issuer {
-		return nil, fmt.Errorf("oidc: issuer did not match the issuer returned by provider, expected %q got %q", issuer, p.Issuer)
-	}
 	return &Provider{
 		issuer:       p.Issuer,
 		authURL:      p.AuthURL,
