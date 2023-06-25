36a57f462d3309e253e9de0d4c4f0d7dec5df267
awwabi <awwabi.labba@gmail.com>
2023-06-25 21:54:03 +0700

    update module name

--- example/userinfo/app.go
+++ example/userinfo/app.go
@@ -13,7 +13,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/dptsi/go-oidc/v3/oidc"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/coreos/go-oidc/v3
+module github.com/dptsi/go-oidc/v3
 
 go 1.19
 
