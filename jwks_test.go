ab6d66b04d9cf1f8c41b0216a2e562fed76c71b2
Jordan Liggitt <liggitt@google.com>
2025-01-22 10:43:06 -0500

    Switch to maintained gopkg.in/go-jose/go-jose.v2 library

--- jwks_test.go
+++ jwks_test.go
@@ -14,7 +14,7 @@ import (
 	"testing"
 	"time"
 
-	jose "gopkg.in/square/go-jose.v2"
+	jose "gopkg.in/go-jose/go-jose.v2"
 )
 
 type keyServer struct {
--- verify.go
+++ verify.go
@@ -13,7 +13,7 @@ import (
 	"time"
 
 	"golang.org/x/oauth2"
-	jose "gopkg.in/square/go-jose.v2"
+	jose "gopkg.in/go-jose/go-jose.v2"
 )
 
 const (
